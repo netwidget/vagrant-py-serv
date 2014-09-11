d62fa4dd0a429144a522a3fd902ea99306f202fa
James H Dugger <james@duggermac-3.local>
2014-09-10 19:29:29 -0700

    Fix- 	Vagrantfile privilege set to true (sudo), modify shell provisioner 	for python, pip, and virtualenv install.  Add additional variables 	for versioning.

--- Vagrantfile
+++ Vagrantfile
@@ -3,14 +3,14 @@
 
 Vagrant.configure("2") do |config|
   config.vm.define :centos do |centos_config|
-    centos_config.vm.box = "centos64-x64"
+    centos_config.vm.box = "centos64_x86_vbg432"
     # If the box has not been added to the local .vagrant.d/boxes folder than comment out line above
     # and uncomment line below.
     #centos_config.vm.box_url = "http://box.puphpet.com/centos64-x64-vbox43.box"
 
   end
   config.vm.provider "virtualbox" do |v|
-    v.name = "kidserv_py" # Change to VM name.
+    v.name = "kidserv" # Change to VM name.
     v.memory = 2048 
   end
   #config.vm.network :private_network, ip: "192.168.33.10"
@@ -25,14 +25,14 @@ Vagrant.configure("2") do |config|
   # Replace <project> with the name of the project folder name. Should match $project_folder
   # centos64-webserv.sh.
   # Mac version
-  #config.vm.synced_folder "/Users/<username>/sitedev/kidwork", "/var/www/kidwork"
+  config.vm.synced_folder "/Users/james/sitedev/kidwork", "/var/www/kidwork"
 
   # Linux version
   #config.vm.synced_folder "/home/<username>/sitedev/kidwork", "/var/www/kidwork"
 
   # Windows version
-  config.vm.synced_folder "/Users/James/Documents/sitedev/kidwork_py", "/var/www/kidwork_py"
+  # config.vm.synced_folder "/Users/James/Documents/sitedev/kidwork", "/var/www/kidwork"
 
   # Provision with BASH
-  config.vm.provision :shell, :path => "centos64-flask.sh"
+  config.vm.provision :shell, privileged: true, :path => "centos64-flask.sh"
 end
--- centos64-flask.sh
+++ centos64-flask.sh
@@ -5,18 +5,35 @@
 # Date: 5-22-14
 # Updated: 6-28/14
 
+#####################################################################
+# Configuration Varisables                                          #
+#####################################################################
+
 # Global Variables.
 # Project folder variable for use in make dir and virtual hosts config file.
 #
-project_folder="kidwork_py"
+project_folder="kidwork"
+
+# CentOS version
+#
+sys_ver_maj="6"
+sys_ver_min="8"
 
 # DocumentRoot variable for virtual host config file.
 #
 doc_root="docroot"
 
-# version of PHP to install. Choice of 5.4 or 5.5.
+# version of python to install.
 #
-php_version="5.4"
+py_ver_maj="2"
+py_ver_min="7"
+py_ver_inc="8"
+
+# Setuptools version
+#
+setup_ver_maj="1"
+setup_ver_min="4"
+setup_ver_inc="2"
 
 # variables for git global config, user, email, editor:
 #
@@ -30,7 +47,7 @@ domain_name="kidwork.dev"
 
 # Set for httpd.conf file to avoid apache warning for FDGN.
 #
-servername="kidserv_py"
+servername="kidserv"
 
 # Default base shared folder. this should match the target folder in the Vagrantfile.
 #
@@ -53,65 +70,150 @@ db_prefix=""
 db_driver_hib="mysql"
 db_charset_hib="utf8"
 
+# nginx config file
+#
+nginx_conf=$'worker_processes 1;\n
+\n
+events {\n
+\n
+    \tworker_connections 1024;\n
+\n
+}\n
+\n
+http {\n
+\n
+    \tsendfile on;\n
+\n
+    \tgzip              on;\n
+    \tgzip_http_version 1.0;\n
+    \tgzip_proxied      any;\n
+    \tgzip_min_length   500;\n
+    \tgzip_disable      "MSIE [1-6]\.";\n
+    \tgzip_types        text/plain text/xml text/csss\n
+                      text/comma-separated-values\n
+                      text/javascript\n
+                      application/x-javascript\n
+                      application/atom+xml;\n
+\n
+    \t# Configuration containing list of application servers\n
+    \tupstream uwsgicluster {\n
+\n
+        \tserver 127.0.0.1:8080;\n
+        \t# server 127.0.0.1:8081;\n
+        \t# ..\n
+        \t# .\n
+\n
+    }\n
+\n
+    \t# Configuration for Nginx\n
+    \tserver {\n
+\n
+        \t\t# Running port\n
+        \t\tlisten 80;\n
+\n
+        \t\t# Settings to by-pass for static files\n 
+        \t\tlocation ^~ /static/  {\n
+\n
+            \t\t\t# Example:\n
+            \t\t\t# root /full/path/to/application/static/file/dir;\n
+            \t\t\troot /app/static/;\n
+\n
+        \t\t}\n
+\n
+        \t\t# Serve a static file (ex. favico) outside static dir.\n
+        \t\tlocation = /favico.ico  {\n
+\n
+            \t\t\troot /app/favico.ico;\n
+\n
+        \t\t}\n
+\n
+        \t\t# Proxying connections to application servers\n
+        \t\tlocation / {\n
+\n
+            \t\t\tinclude            uwsgi_params;\n
+            \t\t\tuwsgi_pass         uwsgicluster;\n
+\n
+            \t\t\tproxy_redirect     off;\n
+            \t\t\tproxy_set_header   Host $host;\n
+            \t\t\tproxy_set_header   X-Real-IP $remote_addr;\n
+            \t\t\tproxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;\n
+            \t\t\tproxy_set_header   X-Forwarded-Host $server_name;\n
+\n
+        }\n
+    }\n
+}\n'
+
+#####################################################################
+# Execution of provisioner                                          #
+#####################################################################
+
 # Update CentOS with latest paths in repositories
 #
 yum update -y
-yum install -y vim git git-core lsof mysql mysql-server mysql-devel autoconf automake wget
 yum groupinstall -y development
-yum install -y zlib-devel openssl-devel sqlite-devel bzip2-devel
+yum install -y vim git git-core lsof mysql mysql-server mysql-devel autoconf automake wget zlib-devel openssl-devel sqlite-devel bzip2-devel
 
 # Turn on networking
 # Set networking to start at boot.
 #
-sed -i 's/^\(ONBOOT\s*=\s*\).*$/\1yes/' /etc/sysconfig/network-scripts/ifcfg-eth0
-echo "Changed ONBOOT=yes"
-ifup eth0
-
+if grep -Fxq "ONBOOT=yes" /etc/sysconfig/network-scripts/ifcfg-eth0
+    then 
+        echo "Network connection already configured."
+    else
+        sed -i 's/^\(ONBOOT\s*=\s*\).*$/\1yes/' /etc/sysconfig/network-scripts/ifcfg-eth0
+        ifup eth0
+        echo "Changed ONBOOT=yes"
+fi
 # Install Python
 #
-wget http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz
+wget http://www.python.org/ftp/python/$py_ver_maj.$py_ver_min.$py_ver_inc/Python-$py_ver_maj.$py_ver_min.$py_ver_inc.tar.xz
 
 
-yum install xz-libs
+yum install -y xz-libs
 
 # Let's decode (-d) the XZ encoded tar archive:
-xz -d Python-2.7.6.tar.xz
+xz -d Python-$py_ver_maj.$py_ver_min.$py_ver_inc.tar.xz
 
 # Now we can perform the extraction:
-tar -xvf Python-2.7.6.tar
+tar -xvf Python-$py_ver_maj.$py_ver_min.$py_ver_inc.tar
 
 # Enter the file directory:
-cd Python-2.7.6
+cd Python-$py_ver_maj.$py_ver_min.$py_ver_inc
 
 # Start the configuration (setting the installation directory)
 # By default files are installed in /usr/local.
 # You can modify the --prefix to modify it (e.g. for $HOME).
 ./configure --prefix=/usr/local  
 
-# Let's build (compile) the source
+# Compile the source
 # This procedure can take awhile (~a few minutes)
 make && make altinstall
 
 # Example: export PATH="[/path/to/installation]:$PATH"
+# Stackoverflow recommends adding the path this way.
+# echo 'pahtmunge /usr/local/bin/python2.7 > /etc/profile.d/python2.7'
 export PATH="/usr/local/bin:$PATH"
 
 # Installing pip
 #
-# Let's download the installation file using wget:
-wget --no-check-certificate https://pypi.python.org/packages/source/s/setuptools/setuptools-1.4.2.tar.gz
+# Download the installation file using wget:
+wget --no-check-certificate https://pypi.python.org/packages/source/s/setuptools/setuptools-$setup_ver_maj.$setup_ver_min.$setup_ver_inc.tar.gz
 
 # Extract the files from the archive:
-tar -xvf setuptools-1.4.2.tar.gz
+tar -xvf setuptools-$setup_ver_maj.$setup_ver_min.$setup_ver_inc.tar.gz
 
 # Enter the extracted directory:
-cd setuptools-1.4.2
+cd setuptools-$setup_ver_maj.$setup_ver_min.$setup_ver_inc
 
-# Install setuptools using the Python we've installed (2.7.6)
-python2.7 setup.py install
+# Install setuptools using the Python we've installed (2.7)
+python$python_ver setup.py install
 
-curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python2.7 -
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 
-
+#Install pip for python version
+#
+python$py_ver_maj.$py_ver_min get-pip.py
+cd /usr/local/bin
 # Installing virtualenv
 #
 pip install virtualenv
@@ -119,131 +221,63 @@ pip install virtualenv
 # Folders / Directories
 
 mkdir -p /$base_folder/$project_folder/app # Application (module) directory
-mkdir -p /$base_folder/$project_folder/env/bin/ # Environment directory
-# Create virtual environment
-#
-cd /$base_folder/$project_folder
+touch /$base_folder/$project_folder/WSGI.py
+touch /$base_folder/$project_folder/app/__init__.python_ver
+cd /$base_folder/$project_folder/
+
+# Setup vritualenv for project.
 virtualenv env
-
-# Download and install uwsgi
 /$base_folder/$project_folder/env/bin/pip install uwsgi
-
-# Download and install Flask library.
 /$base_folder/$project_folder/env/bin/pip install flask
 
 # Create a sample app.
-app='from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello!"
-
-if __name__ == "__main__":
-    app.run()'
+app=$'from flask import Flask\n
+app = Flask(__name__)\n
+\n
+@app.route("/")\n
+def hello():\n
+\treturn "Hello!"\n
+\n
+if __name__ == "__main__":\n
+\tapp.run()\n'
 
 echo $app > /$base_folder/$project_folder/app/__init__.py
 
 # Create a WSGI file.
-wsgi_file='from app import app
-
-if __name__ == "__main__":
-    app.run()'
+wsgi_file=$'from app import app\n
+if __name__ == "__main__":\n
+\tapp.run()'
     
 echo $wsgi_file > /$base_folder/$project_folder/WSGI.py
-
+echo "---------------------------------------------"
+echo "Project files, environement and WSGI setup."
+echo "---------------------------------------------"
 
 # Installing nginx.
-rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/$sys_ver_maj/x86_64/epel-release-$sys_ver_maj-$sys_ver_min.noarch.rpm
 
 yum install -y nginx
 
-# Start nginx
-sudo service nginx start
-
 # Configure nginx
-cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-nginx_conf='worker_processes 1;
+if [ -f /etc/nginx/nginx.conf ]
+    then 
+        mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
 
-events {
 
-    worker_connections 1024;
 
-}
-
-http {
-
-    sendfile on;
-
-    gzip              on;
-    gzip_http_version 1.0;
-    gzip_proxied      any;
-    gzip_min_length   500;
-    gzip_disable      "MSIE [1-6]\.";
-    gzip_types        text/plain text/xml text/css
-                      text/comma-separated-values
-                      text/javascript
-                      application/x-javascript
-                      application/atom+xml;
-
-    # Configuration containing list of application servers
-    upstream uwsgicluster {
-
-        server 127.0.0.1:8080;
-        # server 127.0.0.1:8081;
-        # ..
-        # .
-
-    }
-
-    # Configuration for Nginx
-    server {
-
-        # Running port
-        listen 80;
-
-        # Settings to by-pass for static files 
-        location ^~ /static/  {
-
-            # Example:
-            # root /full/path/to/application/static/file/dir;
-            root /app/static/;
-
-        }
-
-        # Serve a static file (ex. favico) outside static dir.
-        location = /favico.ico  {
-
-            root /app/favico.ico;
-
-        }
-
-        # Proxying connections to application servers
-        location / {
-
-            include            uwsgi_params;
-            uwsgi_pass         uwsgicluster;
-
-            proxy_redirect     off;
-            proxy_set_header   Host $host;
-            proxy_set_header   X-Real-IP $remote_addr;
-            proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header   X-Forwarded-Host $server_name;
-
-        }
-    }
-}'
-
-echo $nginx_conf > /etc/nginx/nginx.conf
-
-sudo service nginx restart
+service nginx restart
+echo "---------------------------------------------"
+echo "Nginx installed."
+echo "---------------------------------------------"
 
 # Start MySQL
 # TODO - set non root user and password for MySQL
 #
 service mysqld start
 chkconfig mysqld on
-echo "MySQL installed and started"
+echo "---------------------------------------------"
+echo "MySQL installed and started."
+echo "---------------------------------------------"
 
 # Create MySQL user and grant privileges.
 # Add database for Drupal site
@@ -280,31 +314,33 @@ git config --global user.name "$username"
 git config --global user.email "$email"
 git config --global  core.editor "$editor"
 git config --list
-echo "Git global settings configured."vagran
+echo "---------------------------------------------"
+echo "Git global settings configured."
+echo "---------------------------------------------"
 
-# IP tables configuration
-#
-/etc/init.d/iptables stop
-iptables -F
-iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
-iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
-iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 25 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 465 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 110 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 995 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 143 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 993 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
-iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -P INPUT DROP
-iptables-save | sudo tee /etc/sysconfig/iptables
-service iptables restart
-echo "iptables configured"
+# # IP tables configuration
+# #
+# /etc/init.d/iptables stop
+# iptables -F
+# iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+# iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+# iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+# iptables -A INPUT -i lo -j ACCEPT
+# iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+# iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+# iptables -A INPUT -p tcp -m tcp --dport 25 -j ACCEPT
+# iptables -A INPUT -p tcp -m tcp --dport 465 -j ACCEPT
+# iptables -A INPUT -p tcp -m tcp --dport 110 -j ACCEPT
+# iptables -A INPUT -p tcp -m tcp --dport 995 -j ACCEPT
+# iptables -A INPUT -p tcp -m tcp --dport 143 -j ACCEPT
+# iptables -A INPUT -p tcp -m tcp --dport 993 -j ACCEPT
+# iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+# iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+# iptables -P OUTPUT ACCEPT
+# iptables -P INPUT DROP
+# iptables-save | sudo tee /etc/sysconfig/iptables
+# service iptables restart
+# echo "iptables configured"
 
 env/bin/uwsgi --socket 127.0.0.1:8088 -w WSGI:app &
  
